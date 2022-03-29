b3be1810e4c8f611ec25863a03c98e9e58a90bc0
4ndu-7h4k <ananduthampi2@gmail.com>
2022-03-29 18:47:12 +0000

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:explore_and_go_application/Screens/Welcome/welcome_screen.dart';
+import 'package:explore_and_go_application/functions/google_sign_in.dart';
 import 'package:explore_and_go_application/home_screen/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyATWJgBjfb7tsK6PG9Am2LqSlbzmI-NNfk",
+        appId: "1:1063181653345:android:d27e4274f3ccefbcb16120",
+        messagingSenderId:
+            "1063181653345-nq11bh75hjq2s4nifu4kekngjor4huqq.apps.googleusercontent.com",
+        projectId: "explore-and-go-project"),
+  );
   runApp(const MyApp());
 }
 
@@ -15,23 +25,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Explore & GO",
-      home: SafeArea(
-        child: StreamBuilder(
-            stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              } else if (snapshot.hasData) {
-                return const HomeScreen();
-              } else {
-                return const WelcomeScreen();
-              }
-            }),
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    return ChangeNotifierProvider(
+      create: (context) => GoogleSignInMethod(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: "Explore & GO",
+        home: SafeArea(
+          child: StreamBuilder(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                } else if (snapshot.hasData) {
+                  return const HomeScreen();
+                } else {
+                  return const WelcomeScreen();
+                }
+              }),
+        ),
       ),
     );
   }

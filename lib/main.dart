7ecac42bec3d96f6baff5c63a04e649afe2330da
4ndu-7h4k <ananduthampi2@gmail.com>
2022-03-29 08:37:31 +0000

    remove api

--- lib/main.dart
+++ lib/main.dart
@@ -6,14 +6,7 @@ import 'package:firebase_core/firebase_core.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyATWJgBjfb7tsK6PG9Am2LqSlbzmI-NNfk",
-        appId: "1:1063181653345:android:d27e4274f3ccefbcb16120",
-        messagingSenderId:
-            "1063181653345-nq11bh75hjq2s4nifu4kekngjor4huqq.apps.googleusercontent.com",
-        projectId: "explore-and-go-project"),
-  );
+  
   runApp(const MyApp());
 }
 
